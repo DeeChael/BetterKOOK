6d4e54bf7baaf5fc6e5fe5d52e075809b003ed22
Deerio Chaelingo <DeeChael@outlook.com>
2023-01-07 19:45:14 +0800

    测试用的js基本能用了

--- betterkook.js
+++ betterkook.js
@@ -35,7 +35,15 @@ class ImageElement extends SettingsElement {
 
 }
 
-class SectionElement extends SettingsElement {
+class FormItem extends DivOwner {
+
+    constructor() {
+        super()
+    }
+
+}
+
+class TextFormItem extends FormItem {
 
     constructor(title, description = null) {
         super()
@@ -54,6 +62,181 @@ class SectionElement extends SettingsElement {
             labelElement.appendChild(descriptionElement)
         }
         this.divElement.appendChild(labelElement)
+    }
+
+    asElement() {
+        return this.divElement
+    }
+
+}
+
+class TextWithSwitchFormItem extends FormItem {
+
+    constructor(title, description = null, status = false) {
+        super()
+        this.status = false
+        this.divElement = document.createElement("div")
+        this.divElement.setAttribute("class", "setting-form-item is-description column")
+        this._resolveText(title, description)
+        this._resolveSwitch(status)
+    }
+
+    _resolveText(title, description) {
+        let labelElement = document.createElement("div")
+        labelElement.setAttribute("class", "setting-form-label")
+        let titleElement = document.createElement("div")
+        titleElement.setAttribute("class", "title")
+        titleElement.innerText = title
+        labelElement.appendChild(titleElement)
+        if (description !== null) {
+            let descriptionElement = document.createElement("div")
+            descriptionElement.setAttribute("class", "description")
+            descriptionElement.innerText = description
+            labelElement.appendChild(descriptionElement)
+        }
+        this.divElement.appendChild(labelElement)
+    }
+
+    _resolveSwitch(status) {
+        let labelElement = document.createElement("div")
+        labelElement.setAttribute("class", "setting-form-input")
+        let spanElement = document.createElement("span")
+        spanElement.setAttribute("class", "ui-switch switchery-small" + (status ? " checked" : ""))
+        spanElement.setAttribute("data-tip", "")
+        spanElement.setAttribute("data-for", "switch-tooltip-")
+        let inputElement = document.createElement("input")
+        inputElement.setAttribute("type", "checkbox")
+        inputElement.setAttribute("readonly", "")
+        inputElement.setAttribute("checked", "")
+        inputElement.setAttribute("style", "display: none;")
+        let subSpanElement = document.createElement("span")
+        subSpanElement.setAttribute("class", "switch-text")
+        spanElement.appendChild(document.createElement("small"))
+        spanElement.appendChild(inputElement)
+        spanElement.appendChild(subSpanElement)
+        labelElement.appendChild(spanElement)
+        this.divElement.appendChild(labelElement)
+    }
+
+    asElement() {
+        return this.divElement
+    }
+
+}
+
+class FormElement extends SettingsElement {
+
+    constructor() {
+        super()
+        this.divElement = document.createElement("div")
+        this.divElement.setAttribute("class", "setting-form")
+    }
+
+    append(formItem) {
+        if (!(formItem instanceof FormItem)) {
+            throw new Error("Not a FormItem")
+        }
+        this.divElement.appendChild(formItem.asElement())
+    }
+
+    asElement() {
+        return this.divElement
+    }
+
+}
+
+class GoodsItem extends DivOwner {
+
+    static MAGENTA_BG = "https://img.kookapp.cn/assets/item/quality_40_small.png"
+ 
+    static PURPLE_BG = "https://img.kookapp.cn/assets/item/quality_30_small.png"
+ 
+    static BLUE_BG = "https://img.kookapp.cn/assets/item/quality_20_small.png"
+
+    static GREEN_BG = "https://img.kookapp.cn/assets/item/quality_10_small.png"
+
+    constructor(bgUrl, imgUrl, name, label, price) {
+        super()
+        this.divElement = document.createElement("div")
+        this.divElement.setAttribute("class", "kprop-goods")
+        this._resolveImage(bgUrl, imgUrl)
+        this._resolveTag()
+        this._resolveDetail(name, label, price)
+    }
+
+    _resolveImage(bgUrl, imgUrl) {
+        let headerElement = document.createElement("div")
+        headerElement.setAttribute("class", "kprop-goods-header")
+        let layerElement = document.createElement("div")
+        layerElement.setAttribute("class", "prop-image-layer")
+        let contentElement = document.createElement("div")
+        contentElement.setAttribute("class", "image-contont-layer")
+        let bgElement = document.createElement("img")
+        bgElement.setAttribute("class", "image-bg-layer")
+        bgElement.setAttribute("src", bgUrl)
+        bgElement.setAttribute("alt", "")
+        let imgElement = document.createElement("img")
+        imgElement.setAttribute("class", "prop-icon")
+        imgElement.setAttribute("draggable", "false")
+        imgElement.setAttribute("src", imgUrl)
+        imgElement.setAttribute("alt", "")
+        contentElement.appendChild(bgElement)
+        contentElement.appendChild(imgElement)
+        layerElement.appendChild(contentElement)
+        headerElement.appendChild(layerElement)
+        this.divElement.appendChild(headerElement)
+    }
+    
+    _resolveTag() {
+        // TODO, 添加一个新类GoodsTag，然后在这里面处理
+        let tagsContainer = document.createElement("div")
+        tagsContainer.setAttribute("class", "kprop-goods-tags")
+        this.divElement.appendChild(tagsContainer)
+    }
+
+    _resolveDetail(name, label, price) {
+        let detailElement = document.createElement("div")
+        detailElement.setAttribute("class", "kprop-goods-detail")
+        let nameElement = document.createElement("div")
+        nameElement.setAttribute("class", "goods-name")
+        nameElement.innerText = name
+        let labelElement = document.createElement("div")
+        labelElement.setAttribute("class", "goods-label")
+        labelElement.innerText = label
+        let goodsPriceElement = document.createElement("div")
+        goodsPriceElement.setAttribute("class", "goods-price")
+        let priceElement = document.createElement("div")
+        priceElement.setAttribute("class", "price")
+        let spanElement = document.createElement("span")
+        spanElement.setAttribute("class", "price-number")
+        spanElement.innerText = price
+        priceElement.appendChild(spanElement)
+        goodsPriceElement.appendChild(priceElement)
+        detailElement.appendChild(nameElement)
+        detailElement.appendChild(labelElement)
+        detailElement.appendChild(goodsPriceElement)
+        this.divElement.appendChild(detailElement)
+    }
+
+    asElement() {
+        return this.divElement
+    }
+
+}
+
+class GoodsListElement extends SettingsElement {
+
+    constructor() {
+        super()
+        this.divElement = document.createElement("div")
+        this.divElement.setAttribute("class", "kprop-shop-goods-list")
+    }
+
+    append(goodsItem) {
+        if (!(goodsItem instanceof GoodsItem)) {
+            throw new Error("Not a GoodsItem")
+        }
+        this.divElement.appendChild(goodsItem.asElement())
     }
 
     asElement() {
@@ -170,8 +353,23 @@ function openSettingsPage() {
 function buildTestSettings() {
     let page = new SettingsPage("Better KOOK 设置", "这里是Better KOOK的设置，与KOOK本身无关")
     let panel = new SettingsPanel("测试用Panel")
-    panel.append(new ImageElement("https://uploadstatic.mihoyo.com/puzzle/upload/puzzle/2022/11/30/546aa4fc739fc12535d19e47dc89c9ee_3039402973052014666.png"))
-    panel.append(new SectionElement("这是title", "这是description"))
+    // panel.append(new ImageElement("https://uploadstatic.mihoyo.com/puzzle/upload/puzzle/2022/11/30/546aa4fc739fc12535d19e47dc89c9ee_3039402973052014666.png"))
+    panel.append(new ImageElement("https://img.kookapp.cn/assets/2023-01/SHdtyz8x5N1680yc.png"))
+    // panel.append(new SectionElement("这是title", "这是description"))
+    let form = new FormElement()
+    form.append(new TextFormItem("这是标题", "这是介绍"))
+    form.append(new TextWithSwitchFormItem("这是一个开关", null, true))
+    panel.append(form)
+    page.append(panel)
+    return page
+}
+
+function buildTestPluginSettings() {
+    let page = new SettingsPage("插件")
+    let panel = new SettingsPanel("已安装的插件")
+    let goodsList = new GoodsListElement()
+    goodsList.append(new GoodsItem(GoodsItem.BLUE_BG, "https://img.kookapp.cn/assets/item/resources/screen_sharingpro_icon.png", "测试插件", "v1.00.0", "作者: DeeChael"))
+    panel.append(goodsList)
     page.append(panel)
     return page
 }
@@ -190,12 +388,16 @@ function buildGroup(groupName, elements) {
     return groupElement
 }
 
+let currentPage = null
+
 function buildGroupElement(elementName, clickFunction) {
     let elementElement = document.createElement('div')
     elementElement.setAttribute("class", "mask-nav-group-item")
     elementElement.onclick = function () {
-        // TODO, 先看上面的TODO，到时候这里需要修改那个变量，并判断是否当前界面已被active，直接return，不重复执行
         elementElement.setAttribute("class", "mask-nav-group-item active")
+        if (elementName == currentPage) { // 防止重复执行
+            return
+        }
         clickFunction()
     }
     let elementNameElement = document.createElement('div')
@@ -214,10 +416,12 @@ function buildGroupElement(elementName, clickFunction) {
             // settingsButton.click() // 先执行原先的代码
             // 下面为添加元素
             setTimeout(function () {
+                currentPage = null
                 let settingsTabList = document.evaluate('/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div/div', document).iterateNext()
                 // let groups = settingsTabList.getElementsByClassName("mask-setting-nav-group") // 获取所有的组
                 let groups = settingsTabList.childNodes // 获取所有的组及分界线
                 let clickers = Array()
+                let clickerss = Array()
                 let position = 1;
                 let newGroups = [];
                 let elements = [];
@@ -226,9 +430,13 @@ function buildGroupElement(elementName, clickFunction) {
                         showSettingsPage(buildTestSettings())
                     }, 1)
                 }))
-                elements.push(buildGroupElement("主题", openSettingsPage))
-                elements.push(buildGroupElement("插件", openSettingsPage))
-                elements.push(buildGroupElement("资源市场", openSettingsPage))
+                // elements.push(buildGroupElement("主题", openSettingsPage)) 暂时做不出来
+                elements.push(buildGroupElement("插件", function() {
+                    setTimeout(function() {
+                        showSettingsPage(buildTestPluginSettings())
+                    }, 1)
+                }))
+                // elements.push(buildGroupElement("资源市场", openSettingsPage)) 没得钱买服务器
                 for (let j = 0; j < elements.length; j++) {
                     clickers.push(elements[j])
                 }
@@ -240,6 +448,7 @@ function buildGroupElement(elementName, clickFunction) {
                     let elementss = groups[i].getElementsByClassName("mask-nav-group-item")
                     for (let j = 0; j < elementss.length; j++) {
                         clickers.push(elementss[j])
+                        clickerss.push(elementss[j])
                     }
 
                 }
@@ -251,7 +460,9 @@ function buildGroupElement(elementName, clickFunction) {
                     clickers[i].onclick = function () {
                         let container = document.evaluate("/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[1]", document).iterateNext()
                         // _clearNodes(container)
-                        _resolveBetterKook(container)
+                        if (clickerss.indexOf(clickers[i]) !== -1) {
+                            _resolveBetterKook(container)
+                        }
                         for (let j = 0; j < clickers.length; j++) {
                             if (clickers[j])
                                 clickers[j].setAttribute("class", "mask-nav-group-item")
